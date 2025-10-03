8a7cf5d8a02d1f1df67dd74fb57d81191528ed83
Nathali Houshmand <nath.houshmand@gmail.com>
2025-10-03 07:45:05 +0200

    Commit efter fel upstått

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,7 +6,9 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 
-
+//
+//Commit #1
+// CLI och bas fixat
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
@@ -47,42 +49,41 @@ public class Main {
             printHelp();
             return;
         }
+//
 
 
+        //Commit #3 - Hämta och analysera priser från API
+        //---
+        
+        List<PricePoint> prices = elpriserAPI.getPrices(zone, date);
 
+        if (prices == null || prices.isEmpty()) {
+            System.out.println("No price data available for " + date + " in zone" + zone);
+            return;
+        }
+        //
 
+        //Commit #4 - visa priser mean, min och max timmar
+        for (PricePoint p : prices) {
+            System.out.printf("%s %.2f öre%n", p.getHourString(), p.price());
+        }
 
+        double mean = prices.stream().mapToDouble(PricePoint::price).average().orElse(0);
+        System.out.printf("Medelpris: %.2f öre%n", mean);
 
+        PricePoint min = prices.stream().min(Comparator.comparingDouble(PricePoint::price)).get();
+        PricePoint max = prices.stream().max(Comparator.comparingDouble(PricePoint::price)).get();
 
+        System.out.printf("Billigast timme: %s %.2f öre%n", min.getHourString(), min.price());
+        System.out.printf("Dyrast timme: %s %.2f öre%n", max.getHourString(), max.price());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //Commit#5
+        if (chargingHours > 0) {
+            double lowestSum = Double.MAX_VALUE;
+            int bestStart = 0;
+            for (int i = 0; i <= prices.size() - chargingHours; i++) {
+                
+        }
 
 
 
@@ -91,6 +92,8 @@ public class Main {
 
     }
 
+    //Commit #3 - printHelp metod implementerad då det inte funkar ovan utan
+     
     private static void printHelp() {
     System.out.println("Usage: java -cp target/classes com.example.Main --zone SE1|SE2|SE3|SE4 [options]");
     System.out.println("--date YYYY-MM-DD   Specify date (default today)");
@@ -98,5 +101,6 @@ public class Main {
     System.out.println("--charging 2h|4h|8h Find optimal charging window");
     System.out.println("--help              Show this help");
 }
+    //
 
 }
